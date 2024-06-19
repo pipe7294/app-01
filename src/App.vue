5351a05e939e7b3adfd3ac0fa39bd1e5d89dc1c0
Felipe Carmona <felipecarmonamaturana@gmail.com>
2024-06-19 00:48:12 -0400

    registroPacientes v1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent="agregarRegistro">
+
+      <div>
+        <label for=""
+          :class="{ 'text-red': registro_input.usuario == null || registro_input.usuario == '' }">Usuario</label>
+        <input type="text" v-model="registro_input.usuario">
+      </div>
+      <div>
+        <label for="" :class="{ 'text-red': registro_input.fecha == null || registro_input.fecha == '' }">Fecha</label>
+        <input type="date" name="" id="" v-model="registro_input.fecha">
+      </div>
+
+      <div>
+        <label for="" :class="{ 'text-red': registro_input.hora == null || registro_input.hora == '' }">Hora</label>
+        <input type="time" name="" id="" v-model="registro_input.hora">
+      </div>
+
+      <div>
+        <label for=""
+          :class="{ 'text-red': registro_input.severidad == null || registro_input.severidad == '' }">Severidad</label>
+        <select name="" id="" v-model="registro_input.severidad">
+          <option value="1">Baja severidad</option>
+          <option value="2">Media severidad</option>
+          <option value="3">Alta severidad</option>
+        </select>
+      </div>
+
+      <div>
+        <label for=""
+          :class="{ 'text-red': registro_input.observaciones == null || registro_input.observaciones == '' }">Obsevaciones</label>
+        <input type="text" v-model="registro_input.observaciones">
+      </div>
+
+      <button :disabled="(registro_input.usuario == null || registro_input.usuario == '') ||
+        (registro_input.fecha == null || registro_input.fecha == '') ||
+        (registro_input.hora == null || registro_input.hora == '') ||
+        (registro_input.severidad == null || registro_input.severidad == '') ||
+        (registro_input.observaciones == null || registro_input.observaciones == '')
+        ">Agregar ticket</button>
+    </form>
+
+    {{ registros }}
+    <p v-if="registros.length == 0">No hay registros</p>
+    <div v-for="(registro, indice) in registros" :key="indice">
+      <CardRegistro :usuario="registro.usuario" :fecha="registro.fecha" :hora="registro.hora"
+        :severidad="registro.severidad" :observaciones="registro.observaciones" @eliminarRegistro="eliminar(indice)" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardRegistro from './components/CardRegistro.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      registros: [],
+      registro_input: {}
+    }
+  },
+  methods: {
+    agregarRegistro() {
+      this.registros.push(this.registro_input)
+      this.registro_input = {};
+    },
+    eliminar(indice) {
+      this.registros.splice(indice, 1)
+    }
+  },
   components: {
-    HelloWorld
+    CardRegistro
   }
 }
 </script>
@@ -24,5 +84,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.text-red {
+  color: red;
 }
 </style>
---
+++ src/components/CardRegistro.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="usuario-card">
+        <div
+            :class="{ 'avatar-container-red': severidad == 3, 'avatar-container-green': severidad == 1, 'avatar-container-yellow': severidad == 2 }">
+            <h3>Usuario</h3>
+            <p>{{ usuario }}</p>
+            <h3>Fecha</h3>
+            <p>{{ fecha }}</p>
+            <h3>Hora</h3>
+            <p>{{ hora }}</p>
+            <h3>Observaciones</h3>
+            <p>{{ observaciones }}</p>
+        </div>
+        <div class="boton-container">
+            <button @click="eliminarUsuario" class="btn-eliminar">Eliminar</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["usuario", "fecha", "hora", "severidad", "observaciones"],
+    methods: {
+        eliminarUsuario() {
+            this.$emit('eliminarRegistro');
+        }
+    }
+}
+</script>
+
+<style scoped>
+.usuario-card {
+
+    border: 1px solid #ccc;
+    margin-bottom: 10px;
+    width: 200px;
+    /* Ancho fijo para mantener el tamaño consistente */
+}
+
+.avatar-container-red {
+    padding: 10px;
+    background-color: red;
+    color: white;
+}
+
+.avatar-container-green {
+    padding: 10px;
+    background-color: green;
+
+}
+
+.avatar-container-yellow {
+    padding: 10px;
+    background-color: yellow;
+}
+
+.avatar {
+    width: 100%;
+    height: auto;
+    border-radius: 5px 5px 0 0;
+    /* Bordes redondeados solo arriba */
+}
+
+.boton-container {
+    text-align: center;
+    padding: 10px;
+}
+
+.btn-eliminar {
+    background-color: #f44336;
+    color: white;
+    border: none;
+    padding: 5px 10px;
+    cursor: pointer;
+}
+</style>
